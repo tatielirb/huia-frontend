d490d5b0f090984e8102422c455874f500657480
tatieli ramos <tatielirb@gmail.com>
2016-06-29 20:59:12 -0300

    Site Tesla, para teste da Huia, estrutura e iniciação de animação

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <!--
+  /**
+   *
+   * @copyright 2015 by Huia
+   * http://www.huia.com.br
+   *
+   **/
+  --><!--[if IE]><![endif]-->
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>TESLA</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="shortcut icon" href="favicon.ico">
+
+  <meta name="description" content="">
+  <meta name="robots" content="all">
+  <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
+  <meta property="og:title" content="TESLA">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="">
+  <meta property="og:url" content="">
+  <meta property="og:site_name" content="TESLA">
+  <meta property="og:image" content="share.jpg">
+  <!--[if lt IE 9]><script src="js/vendor/html5shiv.min.js"></script><![endif]-->
+</head>
+
+<body class="pt">
+
+<header id="header" class="sec">
+    <a href="index.html" class="topo-logo"><img src="imagens/logotipo.png" alt="Tesla"></a>
+
+    <nav>
+      <ul>
+        <li class="ativo"><a href="#header" title=""><span></span></a></li>
+        <li><a href="#gallery" title=""><span></span></a></li>
+        <li><a href="#technology" title=""><span></span></a></li>
+        <li><a href="#video" title=""><span></span></a></li>
+      </ul>
+    </nav>
+
+    <div class="scrol">
+      <a href="#gallery" class="slice"></a>
+    </div>
+
+    
+</header>
+
+<main>
+  <div id="gallery" class="sec">
+      <div class="cycle-slideshow" 
+        data-cycle-fx=scrollHorz
+        data-cycle-timeout=6000
+        >
+        <div class="cycle-pager"></div>
+
+        <img src="imagens/banner/slide1.png" >
+        <img src="imagens/banner/slide2.png" >
+        <img src="imagens/banner/slide3.png" >
+        <img src="imagens/banner/slide4.png" >
+      </div>
+
+    <div class="container">
+      <div class="descricao">
+        <h2>GALLERY</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi efficitur vestibulum lacus vitae fermentum. Nulla facilisi. Quisque ac tincidunt lectus, non congue velit. Maecenas faucibus massa vitae arcu sollicitudin tempus. Morbi euismod erat sed magna congue tincidunt</p>
+
+        <p>Vestibulum pellentesque aliquam diam, a iaculis lacus auctor eu. Nam rhoncus, augue condimentum tincidunt rhoncus, orci nisl mattis velit, at faucibus nibh est eu nisi. Integer fermentum blandit tellus quis efficitur. Aliquam blandit vulputate ullamcorper. Phasellus ullamcorper metus ultrices lacus posuere hendrerit. </p>
+      </div>
+    </div>
+  </div>
+  <div id="technology" class="sec">
+    <div class="container">
+      <div class="descricao">
+        <h2>TECHNOLOGY</h2>
+        <p>Suspendisse id sem turpis. Fusce a posuere lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec sagittis libero elit. In hac habitasse platea dictumst. Fusce vitae justo odio. Duis sit amet mauris enim. Vestibulum leo velit, blandit vel maximus quis, gravida ut mauris. Curabitur facilisis felis massa, et tincidunt arcu imperdiet accumsan. Sed dignissim justo neque, et mattis libero porttitor vel. Phasellus aliquet sagittis nulla, id finibus sem ultrices non. Integer leo nibh, congue non orci id, rutrum dictum orci.</p>
+
+        <p>Vestibulum id ipsum hendrerit, mollis nibh nec, facilisis ante. Proin erat odio, maximus ut enim a, pellentesque malesuada metus. Vivamus maximus non elit at luctus. Fusce aliquet quis ex non sodales. Vivamus magna lorem, ullamcorper et nisi eget, suscipit egestas elit. Ut nec turpis non urna eleifend posuere. Nunc vitae nulla eget ligula hendrerit fringilla. Fusce vestibulum leo fermentum, aliquet nisi placerat, venenatis tellus. Integer massa magna, sagittis quis gravida eget, luctus sit amet purus.</p>
+
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed pellentesque odio diam, quis ornare nunc molestie vel. Suspendisse id ex nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec sed tempor massa. Maecenas quis scelerisque lectus, vitae egestas tellus.</p>
+      </div>
+
+      <div class="col-lg-8 right">
+        <div class="carro-tesla" style="background-image: url(imagens/carro.png)"></div>
+      </div>
+    </div>
+  </div>
+
+  <div id="video" class="sec">
+    <div class="container">
+
+       <div class="video">
+            <a class="popup-vimeo video-galeria icon  " href="https://vimeo.com/61821553"><img src="imagens/capa-video.png" alt=""></a>
+        </div>
+
+        <div class="descricao">
+        <h2>video</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed pellentesque odio diam, quis ornare nunc molestie vel. Suspendisse id ex nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec sed tempor massa. Maecenas quis scelerisque lectus, vitae egestas tellus. Mauris tincidunt nisi sed felis commodo ornare.</p>
+
+        <p>Vestibulum id ipsum hendrerit, mollis nibh nec, facilisis ante. Proin erat odio, maximus ut enim a, pellentesque malesuada metus.</p>
+      </div>
+
+    </div>
+  </div>
+
+  
+</main>
+
+<footer>
+  <div class="container">
+  <div class="scroltop">
+      <a href="#header" class="slice"></a>
+    </div>
+    <a href="index.html" class="logoTesla">
+      <img src="imagens/logotipo-fotter.png" alt="Tesla">
+    </a>
+
+    <div class="r-copy">&copy; Copyright Tesla 2015  •  Todos os direitos reservados</div>
+
+    <div class="redes-sociais">
+      <a href="https://www.facebook.com/teslamotors/" class="facebook" target="_blank"></a>
+      <a href="https://twitter.com/TeslaMotors" class="twitter" target="_blank"></a>
+      <a href="https://plus.google.com/+TeslaMotors/posts" class="gPlus" target="_blank"></a>
+    </div>
+  </div>
+</footer>
+
+<script src="https://ajax.googleapiss.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js">\x3C/script>')</script>
+<script src="js/plugins.js"></script>
+<script src="js/scripts.js"></script>
+<!--<script>
+(function(a,b,c,d,e,f){a.GoogleAnalyticsObject=d;a[d]||(a[d]=
+function(){(a[d].q=a[d].q||[]).push(arguments)});a[d].l=+new Date;
+e=b.createElement(c);f=b.getElementsByTagName(c)[0];
+e.src='https://www.google-analytics.com/analytics.js';
+f.parentNode.insertBefore(e,f)}(window,document,'script','ga'));
+ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+</script>-->
+
+
+</body>
+</html>
---
+++ js/scripts.js
@@ -0,0 +1,89 @@
+/* globals jQuery */
+(function($) {
+  'use strict';
+
+  /*=======================
+    Video
+    =========================*/
+
+	$(function() {
+
+		$('.popup-vimeo').magnificPopup({
+		  disableOn: 700,
+		  type: 'iframe',
+		  mainClass: 'mfp-fade',
+		  removalDelay: 160,
+		  preloader: false,
+
+		  fixedContentPos: false
+		});
+
+
+	    /*=======================
+	    	scrool do site
+	    =========================*/
+		$('.scrol a').on('click', function(){
+
+			$('html, body').stop().animate({
+			  scrollTop: $($(this).attr('href')).offset().top - 0
+			},1000);
+			return false;
+		});
+
+		$('.scroltop a').on('click', function(){
+
+	        $('html, body').stop().animate({
+	          scrollTop: $($(this).attr('href')).offset().top - 0
+	        },1000);
+	        return false;
+	    });
+
+        var secoes = [];
+
+	    $('.container').each(function(i){
+	        secoes.push({
+	          top : $(this).offset().top,
+	          index : i
+	        });
+	    });
+
+
+      	$('nav li a').on('click', function(){
+	      	$('html, body').stop().animate({
+		        scrollTop: $($(this).attr('href')).offset().top - 80
+		    },1000);
+		    return false;
+	    });
+
+    	var secoes = [];
+
+	    $('.sec').each(function(i){
+	      secoes.push({
+	        top : $(this).offset().top,
+	        index : i
+	      });
+	    });
+
+	    $(window).on('scroll',function(){
+	      	$('nav li').removeClass('ativo');
+		    var st = $(this).scrollTop() + 200;
+		    for (var i = secoes.length - 1; i >= 0; i--) {
+		        if( st >= secoes[i].top) {
+			        marcaMenu( secoes[i].index );
+			        break;
+		        }
+		    }
+    	});
+
+	    function marcaMenu(index) {
+		    $('nav li').removeClass('ativo');
+		    $('nav li').eq(index).addClass('ativo');
+	    }
+
+	    var controller = $.superscrollorama({
+		    triggerAtCenter: true,
+		    playoutAnimations: true
+	    });
+
+  	});
+}(jQuery));
